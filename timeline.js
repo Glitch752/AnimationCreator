4693a80ef9e4ca55f9b167af9f5cfebf42a5cd5e
Glitch752 <xxGlitch752xx@gmail.com>
2022-10-31 18:12:06 -0500

    Added basic timeline styles

--- timeline.js
+++ timeline.js
@@ -1,25 +1,37 @@
 function refreshTimeline(objects) {
     let timeline = document.getElementById("timeline");
 
+    let times = "";
+    let totalMinutes = 2;
+
+    for (let i = 0; i < totalMinutes; i++) {
+        let minutes = i;
+        let seconds = 0;
+
+        for(let j = i === 0 ? 1 : 0; j < 60; j++) {
+            if(j % 2 === 1) {
+                times += `
+                    <div class="timeline-column-body-time">
+                        <div class="timeline-column-dark-row"></div>
+                        <span class="timeline-text">${getformattedTime(i, j)}</span>
+                    </div>`;
+            } else {
+                times += `
+                    <div class="timeline-column-body-time">${getformattedTime(i, j)}</div>`;
+            }
+        }
+    }
+
     timeline.innerHTML = `
         <div class="timeline-marker" id="timelineMarker" onpointerdown="mouseDownTimeline()"></div>
         <!-- Time column -->
-        <div class="timeline-column">
+        <div class="timeline-column timeline-column-time">
             <div class="timeline-column-header">
                 <div class="timeline-column-header-title">Time</div>
             </div>
             <div class="timeline-column-body" id="timelineColumnTimes">
-                <!-- <div class="timeline-column-body-time">0</div> -->
-                <div class="timeline-column-body-time">0:01</div>
-                <div class="timeline-column-body-time">0:02</div>
-                <div class="timeline-column-body-time">0:03</div>
-                <div class="timeline-column-body-time">0:04</div>
-                <div class="timeline-column-body-time">0:05</div>
-                <div class="timeline-column-body-time">0:06</div>
-                <div class="timeline-column-body-time">0:07</div>
-                <div class="timeline-column-body-time">0:08</div>
-                <div class="timeline-column-body-time">0:09</div>
-                <div class="timeline-column-body-time">0:10</div>
+                <br />
+                ${times}
             </div>
         </div>
     `;
@@ -44,4 +56,8 @@ function refreshTimeline(objects) {
 
 function mouseDownTimeline() {
     
+}
+
+function getformattedTime(minutes, seconds) {
+    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`.trim();
 }
